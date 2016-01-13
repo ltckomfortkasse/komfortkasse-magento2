e936fc0eb7f0c5d7bdf1259b2536d3c55325681b
machajdik <manuel.machajdik@gmail.com>
2016-01-13 15:45:40 +0100

    added support info

--- Helper/Komfortkasse.php
+++ Helper/Komfortkasse.php
@@ -10,7 +10,7 @@ require_once 'Komfortkasse_Order.php';
  */
 class Komfortkasse
 {
-    const PLUGIN_VER = '1.4.0.4';
+    const PLUGIN_VER = '1.4.1';
     const MAXLEN_SSL = 117;
     const LEN_MCRYPT = 16;
 
