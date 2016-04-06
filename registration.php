85828476985666ab0db6e7f65af745e34fb73b16
machajdik <manuel.machajdik@gmail.com>
2016-04-06 12:39:46 +0200

    changed vendor to lowercase

--- registration.php
+++ registration.php
@@ -1,7 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Ltc_Komfortkasse',
+    'ltc_komfortkasse',
     __DIR__
 );
- 
