a1d561b3b89be70bfdcce14e848b151cf45e546d
kk-gs <gs@komfortkasse.eu>
2016-10-19 20:37:11 +0200

    fixed module name

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'ltc_komfortkasse',
+    'Ltc_Komfortkasse',
     __DIR__
 );
